f853a258952926d55f63eb0267f3aecfac10c4b9
Elatot <113158003+Elashico@users.noreply.github.com>
2024-02-06 17:37:07 +0800

    Update run.py
    to proper inputs

--- run.py
+++ run.py
@@ -1,19 +1,19 @@
-from autoFillMaskWithCandi import(setTokenModel, 
+from autoFillMaskWithCandy import(setTokenModel, 
                       show_mask_fill, 
                       mask_fill_replaced)
 
 input_sentences = [
-    "maling hindi maligo",
-    "maling hindi malego"
+    "Pasensya heto lng ako, bobo sa pagaral",
+    "Pasensya hito lng ako, bobo sa pagaral",
+    "Pasensya heto lng ako, bubo sa pagaral",
+    "Pasensya hito lng ako, bubo sa pagaral"
 ]
 
-modelInput=("GKLMIP/electra-tagalog-base-uncased")
+modelInput=("GKLMIP/bert-tagalog-base-uncased")
 
 setTokenModel(modelInput)
-print("__________________________")
+
 show_mask_fill(input_sentences)
-print("__________________________")
-print(f"{mask_fill_replaced(input_sentences)}")
-print("__________________________ as is")
+
 string = mask_fill_replaced(input_sentences)
 print(string)
