fa68a63b5907012e8c7f29dcb5e3738b6996b68a
Elatot <113158003+Elashico@users.noreply.github.com>
2024-02-11 16:42:16 +0800

    Update __init__.py

--- autoFillMaskWithCandy/__init__.py
+++ autoFillMaskWithCandy/__init__.py
@@ -4,6 +4,6 @@ This package provides functionalities for processing and comparing sentences
 using models from Hugging Face's Transformers library.
 """
 
-__version__ = '0.0.3'
+__version__ = '0.0.4'
 
 from .autoFillMaskWithCandy import setTokenModel, mask_differing_words, show_mask_fill, mask_fill_replaced
